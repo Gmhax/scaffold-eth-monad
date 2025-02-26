db5c9e38dc1e1cd8bc0709e74244b0856ed8ebe1
Gmhax <harvey.metagoons@gmail.com>
2025-02-26 21:31:44 +0800

    Update customChains.ts

--- packages/nextjs/utils/customChains.ts
+++ packages/nextjs/utils/customChains.ts
@@ -2,20 +2,20 @@ import { defineChain } from "viem";
 
 // TODO: Add Chain details here.
 export const monadDevnet = defineChain({
-  id: 41454,
-  name: "Monad Devnet",
-  nativeCurrency: { name: "Monad", symbol: "MON", decimals: 18 },
+  id: 10143,
+  name: "Monad Testnet",
+  nativeCurrency: { name: "Monad Testnet", symbol: "MON", decimals: 18 },
   rpcUrls: {
     default: {
       // TODO: Add Monad RPC URL
-      http: ["<MONAD_RPC_URL>"],
+      http: ["http://testnet-rpc.monad.xyz"],
     },
   },
   blockExplorers: {
     default: {
-      name: "Monad Devnet Blockscout",
+      name: "Monad Testnet Blockscout",
       // TODO: Add Explorer URL
-      url: "<MONAD_BLOCKSCOUT_URL>",
+      url: "http://testnet.monadexplorer>",
     },
   },
 });
